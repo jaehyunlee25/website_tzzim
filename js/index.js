cfa0682c893675f48061c862ab8aa5ec25635b78
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-24 16:11:07 +0900

    'renew'

--- js/index.js
+++ js/index.js
@@ -4,7 +4,23 @@ doc.gcn("inquiry_ok")[0].onclick = function () {
   const type = doc.gbn("inquiryType")[0].value;
   const email = doc.gcn("email")[0].value;
   const content = doc.gbn("inquiryArea")[0].value;
-  log({
+  if (name.trim() == "") {
+    alert("이름을 입력해 주세요.");
+    doc.gcn("name")[0].focus();
+  }
+  if (subject.trim() == "") {
+    alert("제목을 입력해 주세요.");
+    doc.gcn("subject")[0].focus();
+  }
+  if (email.trim() == "") {
+    alert("이메일을 입력해 주세요.");
+    doc.gcn("email")[0].focus();
+  }
+  if (content.trim() == "") {
+    alert("내용을 입력해 주세요.");
+    doc.gbn("inquiryArea")[0].focus();
+  }
+  post({
     name,
     subject,
     content,
