1b36d4c44408757f71db8ddc267354bad83195f9
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-24 16:11:33 +0900

    'renew'

--- js/index.js
+++ js/index.js
@@ -7,18 +7,22 @@ doc.gcn("inquiry_ok")[0].onclick = function () {
   if (name.trim() == "") {
     alert("이름을 입력해 주세요.");
     doc.gcn("name")[0].focus();
+    return;
   }
   if (subject.trim() == "") {
     alert("제목을 입력해 주세요.");
     doc.gcn("subject")[0].focus();
+    return;
   }
   if (email.trim() == "") {
     alert("이메일을 입력해 주세요.");
     doc.gcn("email")[0].focus();
+    return;
   }
   if (content.trim() == "") {
     alert("내용을 입력해 주세요.");
     doc.gbn("inquiryArea")[0].focus();
+    return;
   }
   post({
     name,
