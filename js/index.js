30362fe12e4d80901c5068e371c0886291b4b852
jaehyunlee <jaehyunlee25@gmail.com>
2022-10-24 16:01:52 +0900

    'renew'

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 doc.gcn("inquiry_ok")[0].onclick = function () {
   const name = doc.gcn("name")[0].value;
   const subject = doc.gcn("subject")[0].value;
-  const inquiryType = inquiryType.value;
+  const type = doc.gcn("inquiryType")[0].value;
   const email = doc.gcn("email")[0].value;
-  const inquiryArea = inquiryArea.value;
+  const content = doc.gcn("inquiryArea")[0].value;
   log({
     name,
     subject,
-    inquiryArea,
+    content,
     email,
-    inquiryType,
+    type,
   });
 };
